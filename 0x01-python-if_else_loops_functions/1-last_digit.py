f59d76cd5ada878ee61d2a6ac95188dae28b0107
Aleonogwe Gideon <galosikhena@gmail.com>
2023-06-07 03:55:07 +0100

    program that gets the last digit of a random number and compares it to zero and 5

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+lastN = abs(number) % 10
+
+if lastN > 5:
+    print(f"Last digit of {number} is {lastN} and is greated than 5")
+elif lastN == 0:
+    print(f"Last digit of {number} is {lastN} and is 0")
+else:
+    print(f"Last digit of {number} is {lastN} and is less than 6 and not 0")
