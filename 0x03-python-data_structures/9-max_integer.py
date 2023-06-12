ef0958d1440368cc62441d3fe26a996fba586da0
Gideon Aleonogwe <galosikhena@gmail.com>
2023-06-13 00:45:10 +0100

    prototype that returns the biggest integer of a list and returns None if list is empty

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+returns max integer
+
+Args:
+    maximum is the highest number in the list
+    i is the index number for elements in the list
+
+Returns:
+    The biggest integer of a list and returns None for empty list
+"""
+
+
+def max_integer(my_list=[]):
+    maximum = my_list[0]
+
+    if len(my_list) == 0:
+        return (None)
+    else:
+        for i in my_list:
+            if(i > maximum):
+                maximum = i
+    
+        return maximum
