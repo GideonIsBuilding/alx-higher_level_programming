37af2b019ac82870dc33985a67df2b37065a8957
Gideon Aleonogwe <galosikhena@gmail.com>
2023-06-12 23:29:08 +0100

    function that adds two tupples: first element is a result of the addition of two elements from the original tuples, and so on

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    """
+    Adds two tuples.
+
+    Args:
+        tuple_a: The first tuple.
+        tuple_b: The second tuple.
+    
+    Returns:
+        A tuple with 2 integers:
+        The first element should be the addition of the first element of each argument
+        The second element should be the addition of the second element of each argument
+    """
+
+    if len(tuple_a) == 0:
+        a1, a2 = 0, 0
+    elif len(tuple_a) == 1:
+        a1, a2 = tuple_a[0], 0
+    else:
+        a1, a2 = tuple_a[0], tuple_a[1]
+
+    if len(tuple_b) == 0:
+        b1, b2 = 0, 0
+    elif len(tuple_b) == 1:
+        b1, b2 = tuple_b[0], 0
+    else:
+        b1, b2 = tuple_b[0], tuple_b[1]
+
+    new_tuple = (a1 + b1, a2 + b2)
+
+    return (new_tuple)
