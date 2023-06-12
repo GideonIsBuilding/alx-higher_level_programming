c8677b266ec876628cdac4e141647edb70a24e64
Gideon Aleonogwe <galosikhena@gmail.com>
2023-06-12 14:06:42 +0100

    added spaces to the end

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in range(len(matrix)):
         for column in range(len(matrix[row])):
-            print("{:d}".format(matrix[row][column]), end='')
+            print("{:d}".format(matrix[row][column]), end=' ')
         print()
