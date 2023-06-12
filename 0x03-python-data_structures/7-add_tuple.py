bf6bb95672116d10f691229d50e3bd8006f80d97
Gideon Aleonogwe <galosikhena@gmail.com>
2023-06-12 23:33:25 +0100

    added a condition for when any of the tuples are bigger than 2

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -18,12 +18,16 @@ def add_tuple(tuple_a=(), tuple_b=()):
         a1, a2 = 0, 0
     elif len(tuple_a) == 1:
         a1, a2 = tuple_a[0], 0
+    elif len(tuple_a) == 2:
+        a1, a2 = tuple_a[0], tuple_a[1]
     else:
         a1, a2 = tuple_a[0], tuple_a[1]
 
     if len(tuple_b) == 0:
         b1, b2 = 0, 0
     elif len(tuple_b) == 1:
+        b1, b2 = tuple_b[0], 0
+    elif len(tuple_b) == 2:
         b1, b2 = tuple_b[0], 0
     else:
         b1, b2 = tuple_b[0], tuple_b[1]
