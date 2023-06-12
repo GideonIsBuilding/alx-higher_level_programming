89404f2ba65e46d5573a1ff531c51735b3ab884e
Gideon Aleonogwe <galosikhena@gmail.com>
2023-06-13 00:09:25 +0100

    made the statement after the if condition to return a tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -16,6 +16,6 @@ def multiple_returns(sentence):
     length = len(sentence)
     first = sentence[0]
     if len(sentence) == 0:
-        return (None)
+        return (length, None)
     else:
         return (length, first)
