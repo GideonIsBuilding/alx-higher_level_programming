9fa19c7de1d7e90ffb4bed68caf4a74513cc6a26
Gideon Aleonogwe <galosikhena@gmail.com>
2023-06-17 02:09:40 +0100

    function that returns a list with all values multiplied by a number without using any loops

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
-    if len(my_list) == 0:
-        return (0)
-
-    average = 0
-    size = 0
-    for t in my_list:
-        average += (t[0] * t[1])
-        size += (t[1])
-    return (average / size)
+def mutiply_list_map(my_list=[], number=0):
+    return (list(map(lambda x: x*number, my_list)))
