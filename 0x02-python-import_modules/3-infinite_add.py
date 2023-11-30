0b14a3c24eb6f5614a12c64c14dd010c9a848478
Aleonogwe Gideon <galosikhena@gmail.com>
2023-11-30 12:28:23 +0100

    added if condition to handle the potential index error cases

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
     import sys
     args_number = len(sys.argv) - 1
     sum = 0
-    for i in range(args_number):
-        sum = sum + int(sys.argv[i + 1])
-    print("{}".format(sum))
+    if args_number == 0:
+        print("{}".format(sum))
+    else:
+        for i in range(args_number):
+            sum = sum + int(sys.argv[i + 1])
+        print("{}".format(sum))
