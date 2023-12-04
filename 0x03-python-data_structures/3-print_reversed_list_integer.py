71d881dc83dc115612a5cba05490f3a91f1e5cd0
Aleonogwe Gideon <galosikhena@gmail.com>
2023-12-05 00:43:35 +0100

    edited the formatting specifier

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,4 +4,4 @@ def print_reversed_list_integer(my_list=[]):
     length = len(my_list)
     while length > 0:
         length -= 1
-        print("{}".format(my_list[length]))
+        print("{:d}".format(my_list[length]))
