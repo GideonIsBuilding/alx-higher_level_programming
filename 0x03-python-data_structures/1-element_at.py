d143abb5b6fb424d07583194ff812cbf1a913439
Aleonogwe Gideon <galosikhena@gmail.com>
2023-12-05 00:21:06 +0100

    made the code readable by modifying the conditionality check

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,7 +4,7 @@ def element_at(my_list, idx):
     length = len(my_list)
     if idx < 0:
         return None
-    elif idx > length - 1:
+    elif idx >= length:
         return None
     else:
         return my_list[idx]
