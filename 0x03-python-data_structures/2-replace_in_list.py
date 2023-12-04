622e91e3ccc404daffee51203de5ba3d4f13a77e
Aleonogwe Gideon <galosikhena@gmail.com>
2023-12-05 00:22:44 +0100

    removed the parentheses after the last return function and tweaked teh conditionality check for readability

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,8 +4,8 @@ def replace_in_list(my_list, idx, element):
     length = len(my_list)
     if idx < 0:
         return None
-    elif idx > length - 1:
+    elif idx >= length:
         return None
     else:
         my_list[idx] = element
-    return (my_list)
+    return my_list
