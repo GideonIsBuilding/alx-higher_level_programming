ed796bb980096b281d78bcc8d3f45f57d6e404dc
Aleonogwe Gideon <galosikhena@gmail.com>
2023-12-05 00:08:42 +0100

    deleted the latter part of the code that I had previosly forgot to delete

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -9,7 +9,3 @@ def replace_in_list(my_list, idx, element):
     else:
         my_list[idx] = element
     return (my_list)
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
