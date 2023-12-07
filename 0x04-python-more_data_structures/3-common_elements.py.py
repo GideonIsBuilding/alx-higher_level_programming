ae9a5fda5ceb14cc49fe056bea7a4e142e1881b6
Aleonogwe Gideon <galosikhena@gmail.com>
2023-12-07 02:53:50 +0100

    made the code shorter

--- 0x04-python-more_data_structures/3-common_elements.py.py
+++ 0x04-python-more_data_structures/3-common_elements.py.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    common_set = set()
-
-    for elem in set_1:
-        if elem in set_2:
-            common_set.add(elem)
-    return common_set
+    return set_1 & set_2
