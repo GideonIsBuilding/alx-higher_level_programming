deaffa5c8556d81baa61c7c9e1baec1e925dd668
Aleonogwe Gideon <galosikhena@gmail.com>
2023-12-08 00:11:32 +0100

    made the code 3 lines

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-
-def square_matrix_map(matrix=None):
-    if matrix is None:
-        matrix = []
+def square_matrix_map(matrix=[]):
     return list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
