ccd48c07a5dc7a9229e2c09ed4afae1fe5c7df0e
Aleonogwe Gideon <galosikhena@gmail.com>
2023-12-07 10:28:39 +0100

    removed the value conditionality

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,7 +3,7 @@
 def update_dictionary(a_dictionary, key, value):
     temp_dict = a_dictionary.copy()
     for items in temp_dict.items():
-        if key or value not in temp_dict.items():
+        if key not in temp_dict.items():
             new_dict = {key: value}
             a_dictionary.update(new_dict)
     return a_dictionary
