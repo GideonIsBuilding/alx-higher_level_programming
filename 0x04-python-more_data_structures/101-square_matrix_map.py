03d0e6379d127708724e3cc43d5b1fc4b36bb23a
Aleonogwe Gideon <galosikhena@gmail.com>
2023-12-08 00:10:45 +0100

    used None as teh default and created a new list inside the function

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
-def square_matrix_map(matrix=[]):
+def square_matrix_map(matrix=None):
+    if matrix is None:
+        matrix = []
     return list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
