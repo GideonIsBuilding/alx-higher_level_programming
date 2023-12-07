d8034313f7944153073959511afb4964688cb667
Aleonogwe Gideon <galosikhena@gmail.com>
2023-12-07 10:38:49 +0100

    adpted a more concise approach

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    temp_dict = a_dictionary.copy()
-    for items in temp_dict.items():
-        if key not in temp_dict.items():
-            new_dict = {key: value}
-            a_dictionary.update(new_dict)
+    a_dictionary[key] = value
     return a_dictionary
