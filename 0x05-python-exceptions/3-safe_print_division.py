04b028d75a32ef87325767e3497042b5c40ff7a3
Aleonogwe Gideon <galosikhena@gmail.com>
2023-12-19 13:45:18 +0100

    changed the c variable to result

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,9 +2,9 @@
 
 def safe_print_division(a, b):
     try:
-        c = a / b
+        result = a / b
     except (ZeroDivisionError, TypeError):
-        c = None
+        result = None
     finally:
-        print("Inside result: {}".format(c))
-        return c
+        print("Inside result: {}".format(result))
+        return result
