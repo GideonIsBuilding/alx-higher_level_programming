dc2bb8190aa4767bdfd5cc7e937d45f18a1e2d7c
Aleonogwe Gideon <galosikhena@gmail.com>
2023-12-19 13:39:47 +0100

    only one return statement is used

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,9 +3,8 @@
 def safe_print_division(a, b):
     try:
         c = a / b
-        return c
     except ZeroDivisionError:
         c = None
-        return None
     finally:
         print("Inside result: {}".format(c))
+        return c
