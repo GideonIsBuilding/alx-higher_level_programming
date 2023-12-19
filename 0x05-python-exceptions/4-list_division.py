56593dd7166326d307a1125eae474b676ead07a2
Aleonogwe Gideon <galosikhena@gmail.com>
2023-12-19 14:21:56 +0100

    removed the redundance in from the try block

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,10 +5,9 @@ def list_division(my_list_1, my_list_2, list_length):
 
     for i in range(list_length):
         try:
-            a = (my_list_1[i])
-            b = (my_list_2[i])
+            a = my_list_1[i]
+            b = my_list_2[i]
             result = a / b
-            result_list.append(result)
         except TypeError:
             print("wrong type")
             result = 0
